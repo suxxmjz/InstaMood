3dfce1362bdabec5006ff7bfd9a8e33948df64a2
CarolineBasta <92133996+CarolineBasta@users.noreply.github.com>
2024-06-09 20:14:05 -0600

    find the firefox profile folder more dynamically

--- template-vars.py
+++ template-vars.py
@@ -1,8 +1,16 @@
-#create a file called "vars.py" and copy the following:
+#create a file called "vars.py" and copy the following code:
 import os
+import glob
 
 shortcode = ""
 username = ""
 
-fireFoxProfile = "%APPDATA%/Mozilla/Firefox/Profiles/qc3fw8f0.default-release/cookies.sqlite"
-fireFoxPath = os.path.expandvars(fireFoxProfile)
+firefox_profiles = os.path.expandvars("%APPDATA%/Mozilla/Firefox/Profiles/")
+profile_name = os.path.join(firefox_profiles, "*.default-release")
+full_path = glob.glob(profile_name)
+
+if not full_path:
+    raise Exception("No Firefox profile found.")
+
+fireFoxProfile = full_path[0]
+fireFoxPath = os.path.join(fireFoxProfile, "cookies.sqlite")
